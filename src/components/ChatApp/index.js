598868cb211b4e88d528388ad160f4aa52bb447f
Rihards Okmanis <okmanis8@gmail.com>
2025-02-21 18:55:00 +0100

    feat: implement main chat logic with WebSocket connection
    - Establish WebSocket connection for real-time messaging.
    - Handle sending and receiving chat messages.
    - Integrate basic error handling for connection issues.

--- src/components/ChatApp/index.js
+++ src/components/ChatApp/index.js
@@ -4,6 +4,7 @@ const baseChatImagePath = './assets/images/chat-app';
 
 export default class ChatApp extends DesktopWindow {
   _state;
+  _username = localStorage.getItem('username') || null;
 
   get state() {
     return this._state;
@@ -11,18 +12,51 @@ export default class ChatApp extends DesktopWindow {
 
   set state(value) {
     this._state = value;
+    this.messageListElement = null;
+    this.inputFieldElement = null;
     this.renderBody(value);
   }
+
+  get username() {
+    return this._username || localStorage.getItem('username') || null;
+  }
+
+  set username(value) {
+    this._username = value;
+    localStorage.setItem('username', value);
+  }
+
+  socket;
+  apiKey = 'eDBE76deU7L0H9mEBgxUKVR0VCnq0XBd';
+  channel = 'default_channel';
+  messageListElement;
+  inputFieldElement;
 
   connectedCallback() {
     super.connectedCallback();
     this.state = 'menu';
   }
 
+  disconnectedCallback() {
+    this.socket && this.socket.close();
+  }
+
   renderBody(state) {
+    this.body.style.width = '250px';
+    this.body.style.height = '400px';
+
     if (state === 'menu') {
-      const menu = this.createMenu();
-      this.body.replaceChildren(menu);
+      if (this.username) {
+        this.state = 'chat';
+        return;
+      }
+      this.body.replaceChildren(this.createMenu());
+    } else if (state === 'chat') {
+      const { messageListElement, inputFieldElement, chat } = this.createChat();
+      this.messageListElement = messageListElement;
+      this.inputFieldElement = inputFieldElement;
+      this.body.replaceChildren(chat);
+      this.connect();
     } else {
       throw new Error('Invalid state');
     }
@@ -35,15 +69,85 @@ export default class ChatApp extends DesktopWindow {
 
   handleSubmit(event) {
     event.preventDefault();
-    const formData = new FormData(event.target);
+    const form = event.target;
+
+    switch (form.id) {
+      case 'auth-form':
+        this.handleAuthFormSubmit(form);
+        break;
+      case 'message-form':
+        this.handleMessageFormSubmit(form);
+        break;
+      default:
+        throw new Error('Unhandled form submission');
+    }
+  }
+
+  handleAuthFormSubmit(form) {
+    const formData = new FormData(form);
     const username = formData.get('username');
+    if (username) {
+      this.username = username;
+      this.state = 'chat';
+    }
+  }
+
+  handleMessageFormSubmit(form) {
+    const formData = new FormData(form);
+    const message = formData.get('message');
+    if (message) {
+      this.sendMessage(message);
+      this.inputFieldElement.value = '';
+    }
+  }
+
+  sendMessage(data) {
+    if (!this.socket || this.socket.readyState !== WebSocket.OPEN) {
+      console.error('Cannot send message: WebSocket is not open.');
+      return;
+    }
+
+    const message = {
+      type: 'message',
+      data,
+      username: this.username,
+      channel: this.channel,
+      key: this.apiKey,
+    };
+
+    this.socket.send(JSON.stringify(message));
+  }
+
+  createMessageElement(message) {
+    if (message.username.toLowerCase() === 'the server') {
+      const serverMessageElement = document.createElement('p');
+      serverMessageElement.classList.add('server-message');
+      serverMessageElement.textContent = message.data;
+      return serverMessageElement;
+    } else {
+      const messageElement = document.createElement('div');
+      messageElement.classList.add('message');
+
+      const usernameElement = document.createElement('p');
+      usernameElement.classList.add('username');
+      usernameElement.textContent = `${message.username}:`;
+      messageElement.appendChild(usernameElement);
+
+      const contentElement = document.createElement('p');
+      contentElement.classList.add('content');
+      contentElement.textContent = message.data;
+      messageElement.appendChild(contentElement);
+
+      return messageElement;
+    }
   }
 
   createMenu() {
     const menu = document.createElement('div');
-    menu.classList.add('menu');
+    menu.classList.add('container', 'menu');
 
     const authForm = document.createElement('form');
+    authForm.id = 'auth-form';
     authForm.classList.add('auth');
     authForm.innerHTML = `
       <img src="${baseChatImagePath}/logo.webp" alt="Messenger Logo" />
@@ -56,19 +160,99 @@ export default class ChatApp extends DesktopWindow {
     return menu;
   }
 
+  createChat() {
+    const chat = document.createElement('div');
+    chat.classList.add('container', 'chat');
+
+    const messageListElement = document.createElement('div');
+    messageListElement.classList.add('messages');
+
+    const inputFormElement = document.createElement('form');
+    inputFormElement.id = 'message-form';
+    inputFormElement.classList.add('input-form');
+
+    const inputFieldElement = document.createElement('textarea');
+    inputFieldElement.classList.add('input-field');
+    inputFieldElement.name = 'message';
+    inputFieldElement.required = true;
+    inputFormElement.appendChild(inputFieldElement);
+
+    const sendButton = document.createElement('button');
+    sendButton.type = 'submit';
+    sendButton.classList.add('send-btn');
+    sendButton.innerHTML = '<u>S</u>end';
+    inputFormElement.appendChild(sendButton);
+
+    inputFieldElement.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter' && !e.shiftKey) {
+        e.preventDefault();
+        inputFormElement.requestSubmit();
+      }
+    });
+
+    chat.appendChild(messageListElement);
+    chat.appendChild(inputFormElement);
+
+    return { messageListElement, inputFieldElement, chat };
+  }
+
+  connect() {
+    this.socket = new WebSocket('wss://courselab.lnu.se/message-app/socket');
+    this.socket.addEventListener('open', this.handleOpen);
+    this.socket.addEventListener('message', this.handleMessage.bind(this));
+    this.socket.addEventListener('error', this.handleError);
+    this.socket.addEventListener('close', this.handleClose.bind(this));
+  }
+
+  handleOpen() {
+    console.log('Connection with the server established.');
+  }
+
+  scrollToBottom() {
+    this.messageListElement.scrollTop = this.messageListElement.scrollHeight;
+  }
+
+  handleMessage(event) {
+    const message = JSON.parse(event.data);
+    if (message.type !== 'heartbeat') {
+      const messageElement = this.createMessageElement(message);
+      this.messageListElement.appendChild(messageElement);
+      this.scrollToBottom();
+    }
+  }
+
+  handleClose() {
+    console.log('Connection closed.');
+    this.socket = null;
+  }
+
+  handleError(error) {
+    console.error('Connection error:', error);
+  }
+
   getStyles() {
     return (
       super.getStyles() +
       `
       <style>
-      .menu {
-        position: relative;
-        min-width: 250px;
-        min-height: 400px;
+      .container {
         display: flex;
         flex-flow: column nowrap;
+        min-width: inherit;
+        width: 100%;
+        min-height: inherit;
+        height: 100%;
+        position: relative;
+      }
+
+      .menu {
         align-items: center;
         justify-content: center;
+      }
+
+      .chat {
+        justify-content: space-between;
+        align-items: stretch;
       }
 
       .auth {
@@ -99,6 +283,62 @@ export default class ChatApp extends DesktopWindow {
         opacity: 0.4;
         z-index: -1;
       }
+
+      .messages {
+        overflow-x: hidden;
+        overflow-y: auto;
+        flex: 1 1;
+      }
+
+      .messages .server-message,
+      .messages .username,
+      .messages .content {
+        margin: 0;
+      }
+
+      .messages .server-message {
+        color: #777;
+      }
+
+      .messages .content {
+        margin-left: 1rem;
+      }
+
+      .input-form {
+        flex: 0 0;
+      }
+
+      .input-field {
+        box-sizing: border-box;
+        width: 100%;
+        height: 5em;
+        resize: none;
+        padding-right: 64px;
+        border: 1px solid #ccc;
+        border-radius: 0.25rem;
+        vertical-align: middle;
+        overflow-y: scroll;
+        scrollbar-width: none;
+        -ms-overflow-style: none;
+      }
+
+      .input-field::-webkit-scrollbar {
+        display: none;
+      }
+
+      .input-field:focus-visible {
+        outline: none;
+        border: 1px solid #ccc;
+      }
+
+      .send-btn {
+        width: 52px;
+        height: 48px;
+        position: absolute;
+        right: 9px;
+        bottom: 9px;
+      }
+
       </style>`
     );
   }
