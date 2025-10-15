afd9f3379e471bb1aa28ca49e326182be1d8230d
Henki Papp <pkh@henky.me>
2025-10-15 11:02:39 +0200

    Fix behaviour of Cancel button. Change favicon.

--- src/components/ActivateWindows/index.js
+++ src/components/ActivateWindows/index.js
@@ -9,8 +9,6 @@ const baseActivateWindowsImagePath = '/assets/images/activate-windows';
  */
 export default class ActivateWindows extends DesktopWindow {
   #correctKey = 'FCKGW-RHQQ2-YXRKT-8TG6W-2B7Q8';
-  #form;
-  #inputs;
   #_state;
 
   /**
@@ -39,80 +37,76 @@ export default class ActivateWindows extends DesktopWindow {
   }
 
   /**
-   * Renders the body of the application.
+   * Renders the body of the application and attaches all event listeners.
    */
   renderBody () {
     this.body.style.width = '480px';
     this.body.style.height = '360px';
     this.body.style.position = 'relative';
 
-    const container = document.createElement('div');
-    container.className = 'container';
-    container.innerHTML = `
-      <img src="${baseActivateWindowsImagePath}/installer-icon.png" alt="Installer Icon" class="installer-icon" />
-      <div class="header">
-        <h2>Your Product Key</h2>
-        <p>Type the unique product key for your copy of Windows.</p>
-      </div>
-      <div class="content">
-        <div class="content__left">
-          <img src="${baseActivateWindowsImagePath}/certificate-of-authenticity.png" alt="Certificate of Authenticity" />
+    this.body.innerHTML = `
+      <div class="container">
+        <img src="${baseActivateWindowsImagePath}/installer-icon.png" alt="Installer Icon" class="installer-icon" />
+        <div class="header">
+          <h2>Your Product Key</h2>
+          <p>Type the unique product key for your copy of Windows.</p>
         </div>
-        <div class="content__right">
-          <p>The 25-character product key appears on the lower section of your Certificate of Authenticity.</p>
-          <p>Entering your product key now is optional but strongly recommended to help avoid complications during activation.</p>
+        <div class="content">
+          <div class="content__left">
+            <img src="${baseActivateWindowsImagePath}/certificate-of-authenticity.png" alt="Certificate of Authenticity" />
+          </div>
+          <div class="content__right">
+            <p>The 25-character product key appears on the lower section of your Certificate of Authenticity.</p>
+            <p>Entering your product key now is optional but strongly recommended to help avoid complications during activation.</p>
+          </div>
         </div>
-      </div>
-      <form id="activation-form" class="form">
-        <label for="key-part-1">Product key:</label>
-        <div class="form__inputs">
-          <input type="text" id="key-part-1" name="key-part-1" maxlength="5" required> -
-          <input type="text" id="key-part-2" name="key-part-2" maxlength="5" required> -
-          <input type="text" id="key-part-3" name="key-part-3" maxlength="5" required> -
-          <input type="text" id="key-part-4" name="key-part-4" maxlength="5" required> -
-          <input type="text" id="key-part-5" name="key-part-5" maxlength="5" required>
+        <form id="activation-form" class="form">
+          <label for="key-part-1">Product key:</label>
+          <div class="form__inputs">
+            <input type="text" id="key-part-1" name="key-part-1" maxlength="5" required> -
+            <input type="text" id="key-part-2" name="key-part-2" maxlength="5" required> -
+            <input type="text" id="key-part-3" name="key-part-3" maxlength="5" required> -
+            <input type="text" id="key-part-4" name="key-part-4" maxlength="5" required> -
+            <input type="text" id="key-part-5" name="key-part-5" maxlength="5" required>
+          </div>
+        </form>
+        <div class="footer">
+            <button type="button" id="back-button" disabled>&lt; <u>B</u>ack</button>
+            <button type="submit" form="activation-form" id="next-button"><u>N</u>ext &gt;</button>
+            <button type="button" id="cancel-button">Cancel</button>
         </div>
-      </form>
-      <div class="footer">
-          <button type="button" id="back-button" disabled>&lt; <u>B</u>ack</button>
-          <button type="submit" form="activation-form" id="next-button"><u>N</u>ext &gt;</button>
-          <button type="button" id="cancel-button">Cancel</button>
       </div>
     `;
 
-    this.body.replaceChildren(container);
-
-    this.#form = this.body.querySelector('#activation-form');
-    this.#inputs = this.body.querySelectorAll('.form__inputs input');
+    // --- Event Listener Setup ---
+    const form = this.body.querySelector('#activation-form');
+    const inputs = this.body.querySelectorAll('.form__inputs input');
     const cancelButton = this.body.querySelector('#cancel-button');
 
-    // Attach event listeners after elements are created
-    cancelButton.addEventListener('click', () => this.close());
-    this.#inputs.forEach((input, index) => {
-      input.addEventListener('input', (event) => this.handleInput(event, index));
-      input.addEventListener('keyup', (event) => this.handleKeyup(event, index));
-    });
-  }
+    // Attach all listeners directly after element creation
+    form.addEventListener('submit', (event) => this.handleSubmit(event, inputs));
 
-  /**
-   * Adds event listeners to the application.
-   */
-  addEventListeners () {
-    super.addEventListeners();
-    this.body.addEventListener('submit', this.handleSubmit.bind(this));
+    // Use the inherited method to close the window, just like the 'X' button
+    cancelButton.addEventListener('click', this.handleCloseClick.bind(this));
+
+    inputs.forEach((input, index) => {
+      input.addEventListener('input', (event) => this.handleInput(event, index, inputs));
+      input.addEventListener('keyup', (event) => this.handleKeyup(event, index, inputs));
+    });
   }
 
   /**
    * Handles input events on the key fields.
    * @param {Event} event - The input event.
    * @param {number} index - The index of the input field.
+   * @param {NodeListOf<HTMLInputElement>} inputs - The list of input fields.
    */
-  handleInput (event, index) {
+  handleInput (event, index, inputs) {
     const input = event.target;
     input.value = input.value.toUpperCase();
 
-    if (input.value.length === 5 && index < this.#inputs.length - 1) {
-      this.#inputs[index + 1].focus();
+    if (input.value.length === 5 && index < inputs.length - 1) {
+      inputs[index + 1].focus();
     }
   }
 
@@ -120,24 +114,25 @@ export default class ActivateWindows extends DesktopWindow {
    * Handles keyup events for navigation between input fields.
    * @param {KeyboardEvent} event - The keyup event.
    * @param {number} index - The index of the input field that triggered the event.
+   * @param {NodeListOf<HTMLInputElement>} inputs - The list of input fields.
    */
-  handleKeyup (event, index) {
+  handleKeyup (event, index, inputs) {
     if (event.key === 'Backspace' && event.target.value.length === 0 && index > 0) {
-      this.#inputs[index - 1].focus();
+      inputs[index - 1].focus();
     }
   }
 
   /**
    * Handles form submission.
    * @param {Event} event - The submit event.
+   * @param {NodeListOf<HTMLInputElement>} inputs - The list of input fields.
    */
-  handleSubmit (event) {
+  handleSubmit (event, inputs) {
     event.preventDefault();
-    if (event.target.id !== 'activation-form') return;
+    const enteredKey = Array.from(inputs).map(input => input.value).join('-');
 
-    const enteredKey = Array.from(this.#inputs).map(input => input.value).join('-');
-    if (enteredKey === this.#correctKey) {
-      window.location.href = 'https://henkas.eu';
+    if (enteredKey.toUpperCase() === this.#correctKey) {
+      window.location.href = 'https://henkas.eut';
     } else {
       alert('The product key you entered is not valid.');
     }
